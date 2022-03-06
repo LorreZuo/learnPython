e7975962b9300cf5b112f904cbb3608ef536758e
LorreZuo <34343203+LorreZuo@users.noreply.github.com>
2022-03-06 22:00:59 +0800

    Create randomIp.py

---
+++ randomIp.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+# -*- coding: UTF-8 -*-
+import random
+a=str(random.randint(0,224))+"."
+b=str(random.randint(0,255))+"."
+c=str(random.randint(0,255))+"."
+d=str(random.randint(0,255))
+print(a+b+c+d)
